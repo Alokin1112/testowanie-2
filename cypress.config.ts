d745d3f0d3e8d47776b675773b6349ba6bd58b3d
Dawid Szczepankowski <dawid.szczepankowski@elzab.pl>
2023-12-30 15:04:55 +0100

    added generating reports

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,7 +8,13 @@ export default defineConfig({
     },
     specPattern: "**/*.cy.ts",
   },
-
+  "reporter": "mochawesome",
+  "reporterOptions": {
+    "reportDir": "cypress/reports/mochawesome",
+    "overwrite": false,
+    "html": false,
+    "json": true
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
